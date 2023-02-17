d3ca868edc8765c709cc191394551f0e181e3407
Austin Burk <apburk@gmail.com>
2023-02-17 14:49:25 -0500

    intercept messages, more to save time copying/pasting

--- scripts/extract_netgear_sip_registration_creds.sh
+++ scripts/extract_netgear_sip_registration_creds.sh
@@ -7,7 +7,6 @@ RESULT=$(curl -s 'http://192.168.100.1/MtaEventLogRetail.htm' \
 -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0' \
 -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
 -H 'Accept-Language: en-US,en;q=0.5' \
--H 'Accept-Encoding: gzip, deflate' \
 -H "Authorization: Basic $AUTH" \
 -H 'Connection: keep-alive' \
 -H 'Cookie: XSRF_TOKEN=15871899' \
